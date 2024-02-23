4f6c7ce421ec776d823adf9da2a942934ba71c93
EsmeeIDEMS <esmee@idems.international>
2024-02-23 16:13:24 +0000

    content: 1.1.10

--- config.ts
+++ config.ts
@@ -4,7 +4,7 @@ const config = extendDeploymentConfig({ name: "plh_kids_tz", parent: "plh_kids" 
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/plh-kids-app-tz-content.git",
-  content_tag_latest: "1.1.9",
+  content_tag_latest: "1.1.10",
 };
 
 
