0bc40b1c68dd6fd2019b4e6d7dd591f90d11ce85
Esmee te Winkel <74557272+esmeetewinkel@users.noreply.github.com>
2024-09-20 15:46:47 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -9,10 +9,10 @@ config.git = {
 };
 
 config.google_drive.sheets_folder_ids = [
-  "19wSspWYMbRc75een-kS0q0aq24--75u8", \\ library_app_menu
-  "1UXVz71HniwdtklFnGUEBzzj8ZHI9oQVo", \\ library_field_values
-  "1Y8uC9-rqQtsjQgUfeX9qp-vNzsFDUQFU", \\ kids_global
-  "1xY6e3-VuMiTsVYBV4tPG_CYHoFx1Yq19" \\ kids_tz
+  "19wSspWYMbRc75een-kS0q0aq24--75u8", // library_app_menu
+  "1UXVz71HniwdtklFnGUEBzzj8ZHI9oQVo", // library_field_values
+  "1Y8uC9-rqQtsjQgUfeX9qp-vNzsFDUQFU", // kids_global
+  "1xY6e3-VuMiTsVYBV4tPG_CYHoFx1Yq19" // kids_tz
 ];
 
 config.google_drive.assets_folder_ids = ["1abaL1QGd33NqqLoKuo2t9fVWKmh5ouM9", "1kLl5t3o4A2ssC2iC-lT2bp4u8BZZneSE"];
